8938f52bd3fa07b04ed3d84ab4bd81554370e433
prajnaprabhu3 <prajnapprabhu3@gmail.com>
2026-09-04 12:42:52 +0530

    commit before crxjs migration

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -8,7 +8,7 @@ function forceAscii(): Plugin {
         if (file.type !== "chunk") continue;
         file.code = file.code.replace(
           /[\u0080-\uFFFF]/g,
-          (char) => "\\u" + char.charCodeAt(0).toString(16).padStart(4, "0")
+          (char) => "\\u" + char.charCodeAt(0).toString(16).padStart(4, "0"),
         );
       }
     },
@@ -28,7 +28,6 @@ function dropFontAssets(): Plugin {
   };
 }
 
-
 export default defineConfig({
   plugins: [forceAscii(), dropFontAssets()],
   build: {
@@ -38,12 +37,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         content: "src/content/index.ts",
+        popup:"src/popup/index.html"
       },
       output: {
-        entryFileNames: "content/index.js",
+        entryFileNames: (chunk) =>
+          chunk.name === "content" ? "content/index.js" : "popup/[name].js",
         assetFileNames: "content/[name][extname]",
-        format: "iife",
-inlineDynamicImports: true,
+        format: "es",
+        inlineDynamicImports: true,
       },
     },
   },
